42e9218405baca6043ae89863aa07075fa6ebcbe
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-21 05:44:28 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -18,7 +18,7 @@ class EmailAddressParser
      # @emails = @emails.split(s)
     end
     
-    puts @emails.split("")
+    puts @emails.split(",")
   end
 end
 e = EmailAddressParser.new("john@doe.com, person@somewhere.org")
